89cc62ebe5b481b78cc960ed9d5ab19302f1e69c
Nima Keivan <nimski@gwu.edu>
2013-11-23 02:51:06 -0500

    Added marginalization test code

--- matlab/marginalization.py
+++ matlab/marginalization.py
@@ -1,5 +1,4 @@
 import numpy as np
-from matplotlib import *
 def marginalize_pose(hessian, num_poses, pose_size,
                      num_lm, lm_size, pose_id, lm_ids):
     """
@@ -18,47 +17,37 @@ def marginalize_pose(hessian, num_poses, pose_size,
     # get the number of rows and columns of the hessian
     num_elems = hessian.shape[0]
     pose_elems = num_poses * pose_size
-
-    # this index points to the current location of the marginalized items in
-    # the hessian
-    pointer_idx = num_elems;
+    
+    # first shift the landmarks to the end
+    lm_cols = hessian[:, lm_ids + pose_elems];
+    hessian = np.delete(hessian, lm_ids + pose_elems, 1);
+    hessian = np.lib.index_tricks.c_[hessian, lm_cols];
+    
+    lm_rows = hessian[lm_ids + pose_elems, :];
+    hessian = np.delete(hessian, lm_ids + pose_elems, 0);
+    hessian = np.lib.index_tricks.r_[hessian, lm_rows];
 
     # get the index of the pose in the hessian
     pose_idx = pose_id * pose_size
-
-    # create the from/to indices for moving the pose columns/rows to the
-    # bottom right of the matrix
-    from_ids = np.arange(pose_idx, pose_idx + pose_size)
-    to_ids = np.arange(pointer_idx - pose_size, pointer_idx)
+    
+    #get the index of where the pose will end up
+    final_pose_idx = num_elems - pose_size;
 
     # swap the rows/columns in the hessian
-    hessian[:, [from_ids, to_ids]] = hessian[:, [to_ids, from_ids]]
-    hessian[[from_ids, to_ids], :] = hessian[[to_ids, from_ids], :]
-
-    # update the pointer after the pose movement
-    pointer_idx -= pose_size
-
-    # go through each landmark and move it to the appropriate location
-    for lm_id in lm_ids:
-        lm_idx = pose_elems + lm_id * lm_size
-        # create the from/to arrays for the move
-        from_ids = np.arange(lm_idx, lm_idx + lm_size)
-        to_ids = np.arange(pointer_idx - lm_size, pointer_idx)
-
-        # swap the rows/columns in the hessian
-        hessian[:, [from_ids, to_ids]] = hessian[:, [to_ids, from_ids]]
-        hessian[[from_ids, to_ids], :] = hessian[[to_ids, from_ids], :]
-
-        # update the pointer
-        pointer_idx -= lm_size
+    idx = np.arange(pose_idx, num_elems)
+    hessian[:, idx] = np.roll(hessian[:, idx], 
+                             final_pose_idx - pose_idx, 1)
+    hessian[idx, :] = np.roll(hessian[idx, :], 
+                             final_pose_idx - pose_idx, 0)
 
     # once we have the pose/landmars in the correct spot, we want to perform
     # the schur complement: U - W*V^-1*W
     schur_elems = num_elems - (pose_size + lm_ids.shape[0] * lm_size)
     # U = hessian[0 : num_elems, 0 : num_elems]
-    W = hessian[0 : num_elems, num_elems : ]
-    V = hessian[num_elems : , num_elems :]
-    prior = W.dot(inv(V)).dot(W.T);
+    W = hessian[0 : schur_elems, schur_elems : ]
+    V = hessian[schur_elems : , schur_elems :]
+    prior = W.dot(np.linalg.inv(V)).dot(W.T);
+    pass
 
 
 
--- matlab/test_marginalization.py
+++ matlab/test_marginalization.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import marginalization as mg
 import numpy as np
-from matplotlib import *
 def marginalize_pose_test():
     """
     Test function for testing the marginalize_pose function.
@@ -18,6 +17,8 @@ def marginalize_pose_test():
                         pose_id, lm_ids)
 
 def marginalize_pose_csv_test():
+    proj_pose_size = 6;
+    pose_size = 9;
     u = np.genfromtxt("/Users/nimski/Code/Build/rslam/Applications/"
         "RelativeStereoSlam/u_orig.txt")
     w = np.genfromtxt("/Users/nimski/Code/Build/rslam/Applications/"
@@ -25,9 +26,9 @@ def marginalize_pose_csv_test():
     # since w is always operating on 6 pose elements, we must pad it
     # horizontally
     w_padded = np.zeros([u.shape[0], w.shape[1]])
-    c = np.array([range(i, i+6)
+    c = np.array([range(i, i + proj_pose_size)
                  for i in range(0, u.shape[0])
-                 if i % 9 == 0]).flatten()
+                 if i % pose_size == 0]).flatten()
     w_padded[c , :] = w;
 
     v = np.genfromtxt("/Users/nimski/Code/Build/rslam/Applications/"
@@ -35,14 +36,14 @@ def marginalize_pose_csv_test():
     # construct the hessian
     hessian = np.r_[np.c_[u, w_padded], np.c_[w_padded.T, v]]
     # the total number of poses in the hessian
-    num_poses = u.shape[0] / 9
+    num_poses = u.shape[0] / pose_size
     # the pose id of the pose to be marginalized
     pose_id = num_poses - 1
     # we must now obtain the id of any landmark that was seen in this pos
-    lm_ids = np.where(w[pose_id * 6, :] != 0);
-    mg.marginalize_pose(hessian, num_poses, 9, w.shape[1], 1 ,
+    lm_ids = w_padded[pose_id * pose_size, :].nonzero()[0]
+    mg.marginalize_pose(hessian, num_poses, pose_size, w.shape[1], 1 ,
                         num_poses-1, lm_ids)
     pass
 
 # run the test
-#marginalize_pose_csv_test();
+marginalize_pose_csv_test();
